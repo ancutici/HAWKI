4a46355f2e1bb379ded0313b9f290baba7acc2ae
Michael Ancutici <ancutici@uni-hohenheim.de>
2024-06-05 10:00:27 +0200

    Logo und Modell

--- private/pages/interface.php
+++ private/pages/interface.php
@@ -36,6 +36,14 @@
         exit();
 
     }
+
+	// Load the environment variables
+	if (file_exists(ENV_FILE_PATH)){
+		$env = parse_ini_file(ENV_FILE_PATH);		
+	}
+
+	// Set the GPT model to use
+	$GPT_MODEL = isset($env) ? $env['GPT_MODEL'] : 'gpt-4o';
 ?>
 <!DOCTYPE html>
 <head>
@@ -92,6 +100,7 @@
 				<summary>
 					<h3><?php echo $translation["Conversation"]; ?>
 						<svg viewBox="0 0 50 50"><path d="M 25 2 C 12.309295 2 2 12.309295 2 25 C 2 37.690705 12.309295 48 25 48 C 37.690705 48 48 37.690705 48 25 C 48 12.309295 37.690705 2 25 2 z M 25 4 C 36.609824 4 46 13.390176 46 25 C 46 36.609824 36.609824 46 25 46 C 13.390176 46 4 36.609824 4 25 C 4 13.390176 13.390176 4 25 4 z M 25 11 A 3 3 0 0 0 22 14 A 3 3 0 0 0 25 17 A 3 3 0 0 0 28 14 A 3 3 0 0 0 25 11 z M 21 21 L 21 23 L 22 23 L 23 23 L 23 36 L 22 36 L 21 36 L 21 38 L 22 38 L 23 38 L 27 38 L 28 38 L 29 38 L 29 36 L 28 36 L 27 36 L 27 21 L 26 21 L 22 21 L 21 21 z"/></svg>
+						<span style="font-size: small;"><?php echo $translation["model"]; ?> <?= $GPT_MODEL ?></span>
 					</h3>
 				</summary>
 				<?php echo $translation["Conversation_Info"]; ?>
@@ -375,7 +384,7 @@
 		document.querySelector('.limitations')?.remove();
 
 		const requestObject = {};
-		requestObject.model = 'gpt-4-turbo-preview';
+		requestObject.model = "<?= $GPT_MODEL ?>"; //'gpt-4-turbo-preview';
 		requestObject.stream = true;
 		requestObject.messages = [];
 		const messageElements = messagesElement.querySelectorAll(".message");
